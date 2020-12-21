f0af54c0b4ec98cf6869ef695ba0253c72072bbb
GrapyGit <76045081+GrapyGit@users.noreply.github.com>
2020-12-21 10:17:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,54 @@
-const pogoda = require('./cityboo')
 global.fetch = require("node-fetch");
 const config = require("config");
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(config.get('token'))
-
-bot.start( ctx => ctx.reply(`
+bot.start(ctx => ctx.reply(`
 Привет ${ctx.from.first_name}!
 Узнай погоду в интересующем тебя городе.
-Введи название города и получи температуру.
-`))
-
+Введи название города  и получи температуру.
+`)
+)
 bot.help((ctx) => ctx.reply(`
-   Есть много городов и есть один бот
-   Федя, самое главное зонт не забудь
-   Есть вопросы вводи команду /voprosy
-`))
-
+Есть много городов и есть один бот
+Федя, самое главное зонт не забудь
+Есть вопросы вводи команду /voprosy
+`)
+)
+bot.hears('мотивируй меня', ctx => {
+  ctx.replyWithPhoto(
+    'https://cs8.pikabu.ru/post_img/2017/03/21/6/og_og_1490083518283917583.jpg',
+    {
+      caption: ctx.reply('ну тип будь сильным')
+    }
+  )
+})
 bot.hears('кто такой Федя?', (ctx) => ctx.reply('Тоби забанить'))
-
 bot.command('voprosy', (ctx) => ctx.reply(
   `
-  За достоверность информации не вручаюсь
-  Потому что бот использует сторонее aip.
-  Вопросы не ко мне :)
+За достоверность информации не ручаюсь
+Потому что бот использует стороннее aip.
+Вопросы не ко мне :)
   `
 ))
-
 bot.on('text', async (ctx) => {
-  if (ctx.message.text.toLowerCase() in pogoda) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + pogoda[ctx.message.text.toLowerCase()] + ',ru&lang=ru&appid=' + config.get('apitoken') + '&units=metric')
-      .then(Response => Response.json())
-      .then(data => {
-        ctx.reply(`
-Город: ${data['name']}
+  fetch(encodeURI('https://api.openweathermap.org/data/2.5/weather?q=' + ctx.message.text.toLowerCase() + '&lang=ru&appid=' + config.get('apitoken') + '&units=metric'))
+    .then(Response => Response.json())
+    .then(data => {
+      ctx.replyWithPhoto("http://openweathermap.org/img/wn/" + data['weather'][0]['icon'] + "@4x.png",
+        {
+          caption: `
+Город:${data['name']}
+        
 Температура: ${data['main']['temp']} °C
 Скорость ветра: ${data['wind']['speed']} м/c  
 На небе: ${data['weather'][0]['description']}
-Влажость: ${data['main']['humidity']} %
-`)
-        ctx.replyWithPhoto("http://openweathermap.org/img/wn/" + data['weather'][0]['icon'] + "@2x.png");
-      })
-      .catch(err => ctx.reply("Не понятная ошибка LoL"))
-  }
-  else ctx.reply("Нет такого города " + ctx.message.text + " :(")
+Влажность: ${data['main']['humidity']} %`
+        }
+      )
+    }
+    )
+    .catch(err => ctx.reply("Непонятная ошибка LoL"))
 })
-
-bot.on('sticker', (ctx) => ctx.reply('Я не понимаю что ты иммеешь ввиду :(')) 
-
+bot.on('sticker', (ctx) => ctx.reply('Я не понимаю что ты имеешь ввиду :('))
 bot.launch()
+
